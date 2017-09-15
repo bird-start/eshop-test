11414380818879929456620322271b37bd473eca
aa598741 <977548166@qq.com>
2017-09-15 17:54:24 +0800

    tijiao

--- src/pages/detail/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-export default class extends React.Component {
-  render() {
-    return <div>this is detail page, ID is {this.props.match.params.itemId}</div>;
-  }
-}
-
